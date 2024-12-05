32b39e59d9df681b6bc1c9280a5bc142d92a0b47
RicardoBarrote <ricardoambscfc@gmail.com>
2024-12-05 13:52:21 -0300

    Atualizando dados cadastrais do paciente

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,10 @@
 const url = "http://localhost:8080/pacientes"
+
 const respostaCriarPaciente = "Paciente cadastrado!"
 const erroAoCriarPaciente = "Não foi possível cadastrar o paciente, tente novamente."
+const respostaAtualizarPaciente = "Sucesso! dados cadastrais foram atualizados."
 
-function postUser(paciente) {
+function postPaciente(paciente) {
     axios.post(url, paciente)
         .then(response => {
             console.log(response.data)
@@ -28,7 +30,40 @@ document.addEventListener('DOMContentLoaded', () => {
             preferencial: preferencial
         };
 
-        postUser(paciente);
+        postPaciente(paciente);
     });
 });
 
+
+function putPaciente(id, paciente){
+    const urlAtualizar =  `http://localhost:8080/pacientes/${id}`;
+    axios.put(urlAtualizar, paciente)
+    .then(response => {
+        console.log(response.data);
+        alert(respostaAtualizarPaciente);
+    })
+    .catch(error => {
+        console.error("Erro ao atualizar os dados cadastrais do paciente: ", error)
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const atualizarPaciente = document.getElementById('atualizarPaciente');
+
+    atualizarPaciente.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const id = parseInt(document.getElementById('id').value);
+        const nome = document.getElementById('atualizarNome').value;
+        const preferencial = document.getElementById('atualizarStatusPreferencial').checked;
+
+        const paciente = {
+            nome: nome,
+            preferencial: preferencial
+        };
+
+        putPaciente(id, paciente);
+    });
+});
+
+func
